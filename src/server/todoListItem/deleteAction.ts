e708b4167f7ddc86e999f4770855507e8a23993e
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-07 11:34:10 +0900

    削除機能

---
+++ src/server/todoListItem/deleteAction.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { eq } from "drizzle-orm";
+import { redirect } from "next/navigation";
+import { db } from "../db";
+import { todos } from "../db/schema";
+
+export async function deleteAction(id: number, formData: FormData) {
+  console.log("deleteAction");
+  console.log("id", id);
+
+  try {
+    const result = await db.delete(todos).where(eq(todos.id, id));
+
+    console.log("result", result);
+  } catch (error) {
+    console.log(error);
+  }
+
+  redirect("/");
+}
