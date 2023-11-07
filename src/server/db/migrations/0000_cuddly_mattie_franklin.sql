b512e89c9130ccd1cd39b1b3112960b1290530b0
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-07 11:01:17 +0900

    delete

--- src/server/db/migrations/0000_cuddly_mattie_franklin.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `todos` (
-	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
-	`user_id` text NOT NULL,
-	`title` text NOT NULL,
-	`is_done` integer DEFAULT false NOT NULL,
-	`created_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL,
-	`updated_at` text DEFAULT CURRENT_TIMESTAMP NOT NULL
-);
