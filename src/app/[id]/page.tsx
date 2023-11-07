36ef27a3e35463561acbe363037c8794ef826c98
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-07 16:45:35 +0900

    wip

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,14 +1,22 @@
+import { CommentForm } from "@/components/CommentForm";
+import { CommentList } from "@/components/CommentList";
 import { getTodo } from "@/server/todoList/todo";
 
-export default async function Home({ params }: { params: { id: number } }) {
+export default async function Home({ params }: { params: { id: string } }) {
   const todo = await getTodo(params.id);
   console.log(todo);
 
   return (
-    <div>
-      <div>{params.id}</div>
+    <div className="flex justify-center">
+      <div className="w-2/3 flex flex-col items-center">
+        <div className="text-xl">{todo?.title}</div>
 
-      <div>{todo?.title}</div>
+        <CommentForm todoId={params.id} />
+
+        <div className="my-3">
+          <CommentList todoId={params.id} />
+        </div>
+      </div>
     </div>
   );
 }
